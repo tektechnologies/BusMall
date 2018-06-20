a44f371c1f43f03029da679089c40b300d2f6b48
tektechnologies <info@tektechnologies.com>
2018-06-19 23:03:34 -0500

    this adds the canvas

--- js/busGus.js
+++ js/busGus.js
@@ -5,7 +5,7 @@ var nextImage = 0;
 function displayImages() {
   // Display image1
   var image1 = Placeholder.all[nextImage++];
- 
+
   var img1 = document.getElementById('product-1');
   img1.src = image1.src;
   img1.currentPlaceholder = image1;
@@ -48,3 +48,69 @@ new Placeholder('', '');
 console.log('Voting Images', Placeholder.all);
 
 window.addEventListener('load', displayImages);
+
+
+
+////////////////////////////////
+//This starts the Canvas js ///
+function showVotingResultChart(){
+  var canvas = document.getElementById('resultsCanvas');
+
+  //Un-hide our Canvas
+  canvas.style.display = 'block';
+  
+  var labels = [];
+  var voteCounts = [];
+  var showCounts = [];
+  var votePercentage = []:
+
+  for(var i = 0; i < Placeholder.all.length; i++){
+    labels[i] = Placeholder.all[i].name;
+    voteCounts[i] = Placeholder.all[i].voteCount;
+    showCounts[i] = Placeholder.all[i].showCount;
+    votePercentage[i] = 100 * voteCounts[i] / showCounts[i];
+
+  }
+
+var ctx = canvas.msGetInputContext('2d');
+var chart = new chart(ctx, {type: 'bar', 
+data: {
+  labels: labels,
+  datasets: [{
+    label: 'Show Count',
+    backgroundColor: 'rgb(200,0,0,0.6)',
+    data: voteCounts
+  },
+{
+  label:'Show Count',
+  backgroundColor: 'rgb(0,0,200,0.4)',
+  data: showCounts
+},
+{
+  label: 'Vote %',
+  data: votePercentage
+}
+]
+},
+options:{
+  responsive: true,
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  },
+  title: {
+    display: true,
+    text: 'Voting Results'
+  }
+}
+});
+}
+
+
+
+
+
+}
